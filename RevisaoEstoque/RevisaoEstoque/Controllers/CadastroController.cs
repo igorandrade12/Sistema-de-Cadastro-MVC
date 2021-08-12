e47b346d4485be5d23ff611d21b02e7b08c434f0
Igor Andrade <55281934+igorandrade12@users.noreply.github.com>
2021-08-12 16:33:02 -0300

    Sistema de Cadastro MVC
    Projeto de cadastro de produtos e clientes criado em ASP.NET MVC ( C# ) seguindo os vídeos do Raniere Silva no Youtube.

---
+++ RevisaoEstoque/RevisaoEstoque/Controllers/CadastroController.cs
@@ -0,0 +1,114 @@
+﻿using RevisaoEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RevisaoEstoque.Controllers
+{
+    public class CadastroController : Controller
+    {
+        private static List<GrupoProdutoModel> _listaGrupoProduto = new List<GrupoProdutoModel>()
+        {
+            new GrupoProdutoModel() {Id=1, Nome="Livros", Ativo=true},
+            new GrupoProdutoModel() {Id=2, Nome="Mouses", Ativo=true},
+            new GrupoProdutoModel() {Id=3, Nome="Monitores", Ativo=false}
+        };
+        [HttpPost]
+        [Authorize]
+        public ActionResult RecuperarGrupoProduto(int id)
+        {
+            return Json(_listaGrupoProduto.Find(x => x.Id == id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult ExcluirGrupoProduto(int id)
+        {
+            var ret = false;
+            var registroBD = _listaGrupoProduto.Find(x => x.Id == id);
+            if(registroBD != null)
+            {
+                _listaGrupoProduto.Remove(registroBD);
+                ret = true;
+            }
+            return Json(ret);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult SalvarGrupoProduto(GrupoProdutoModel model)
+        {
+            var registroBD = _listaGrupoProduto.Find(x => x.Id == model.Id);
+            if (registroBD == null)
+            {
+                registroBD = model;
+                registroBD.Id = _listaGrupoProduto.Max(x => x.Id);
+                _listaGrupoProduto.Add(registroBD);
+            }
+            else
+            {
+                registroBD.Nome = model.Nome;
+                registroBD.Ativo = model.Ativo;
+            }
+            return Json(registroBD);
+        }
+
+        [Authorize]
+        public ActionResult GrupoProduto()
+        {
+            return View(_listaGrupoProduto);
+        }
+        [Authorize]
+        public ActionResult MarcaProduto()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult LocalProduto()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult UnidadeMedida()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Produto()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Pais()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Estado()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Cidade()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Fornecedor()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult PerfilUsuario()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Usuario()
+        {
+            return View();
+        }        
+    }
+}
